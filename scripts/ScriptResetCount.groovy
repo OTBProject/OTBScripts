3bff12feca789410353746c7003cd92f64a18785
NthPortal <mail.vanpelt@gmail.com>
2015-03-07 21:23:01 -0500

    Fixed typo in rename script. Added reset count script.

--- scripts/ScriptResetCount.groovy
+++ scripts/ScriptResetCount.groovy
@@ -1,6 +1,4 @@
 import com.github.otbproject.otbproject.commands.Command
-import com.github.otbproject.otbproject.commands.loader.CommandLoader
-import com.github.otbproject.otbproject.commands.loader.LoadedCommand
 import com.github.otbproject.otbproject.messages.send.MessagePriority
 import com.github.otbproject.otbproject.proc.ScriptArgs
 import com.github.otbproject.otbproject.util.BuiltinCommands
@@ -21,16 +19,13 @@ public boolean execute(ScriptArgs sArgs) {
         return false;
     }
 
-    LoadedCommand command = Command.get(sArgs.db, sArgs.argsList[0]);
-    if (sArgs.userLevel.getValue() < command.modifyingUserLevels.getNameModifyingUL().getValue()) {
-        String commandStr = BuiltinCommands.GENERAL_INSUFFICIENT_USER_LEVEL + " " + sArgs.commandName + " change name of command '" + sArgs.argsList[0] + "'";
+    if (sArgs.userLevel.getValue() < Command.get(sArgs.db, sArgs.argsList[0]).modifyingUserLevels.getResponseModifyingUL().getValue()) {
+        String commandStr = BuiltinCommands.GENERAL_INSUFFICIENT_USER_LEVEL + " " + sArgs.commandName + " reset count of command '" + sArgs.argsList[0] + "'";
         ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
         return false;
     }
 
-    command.setName(sArgs.argsList[1]);
-    Command.remove(sArgs.db, sArgs.argsList[0])
-    CommandLoader.addCommandFromLoadedCommand(sArgs.db, command);
+    Command.resetCount(sArgs.db, sArgs.argsList[0]);
     // TODO run some success command
     return true;
 }
