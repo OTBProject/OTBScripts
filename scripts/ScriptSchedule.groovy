b2291dc6aeac0fef04fc5b8ad21ce3a46613ebd6
Justin Wiblin <justin_wiblin@hotmail.co.uk>
2015-05-28 04:28:56 +0100

    Changes to the Scheduling scripts,merged into one.
    Signed-off-by: Justin Wiblin <justin_wiblin@hotmail.co.uk>

--- scripts/ScriptSchedule.groovy
+++ scripts/ScriptSchedule.groovy
@@ -1,30 +1,58 @@
-import com.github.otbproject.otbproject.api.APIChannel
 import com.github.otbproject.otbproject.api.APISchedule
 import com.github.otbproject.otbproject.commands.Command
+import com.github.otbproject.otbproject.messages.send.MessagePriority
 import com.github.otbproject.otbproject.proc.ScriptArgs
 import com.github.otbproject.otbproject.util.BuiltinCommands
 import com.github.otbproject.otbproject.util.ScriptHelper
-import com.github.otbproject.otbproject.messages.send.MessagePriority
 
 import java.util.concurrent.TimeUnit
+import java.util.stream.Collectors
 
 public class ResponseCmd {
     public static final String SUCCESSFULL_SCHEDULE = "~%schedule.success";
     public static final String GENERAL_DOES_NOT_EXIST = "~%command.general:does.not.exist";
     public static final String COMMAND_ALREADY_SCHEDULED = "~%schedule.already.scheduled";
-
+    public static final String SUCCESSFULL_UNSCHEDULE = "~%unschedule.success";
+    public static final String COMMAND_NOT_SCHEDULED = "~%unschedule.not.scheduled";
+}
+public boolean execute(ScriptArgs sArgs) {
+    if (sArgs.argsList.length < 1) {
+        String commandStr = BuiltinCommands.GENERAL_INSUFFICIENT_ARGS + " " + sArgs.commandName;
+        ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
+        return false;
+    }
+    switch (sArgs.argsList[0].toLowerCase()) {
+        case "add":
+        case "new":
+            return schedule(sArgs);
+        case "remove":
+        case "delete":
+        case "del":
+        case "rm":
+            return unSchedule(sArgs);
+        case "list":
+            Set set = APISchedule.getScheduledCommands(sArgs.channel)
+            List list = set.stream().sorted().collect(Collectors.toList())
+            String asString = "Scheduled commands: " + list.toString();
+            ScriptHelper.sendMessage(sArgs.destinationChannel, asString, MessagePriority.HIGH);
+            return true;
+        default:
+            String commandStr = BuiltinCommands.GENERAL_INVALID_ARG + " " + sArgs.commandName + " " + action;
+            ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
+            return false;
+    }
 }
 
-public boolean execute(ScriptArgs sArgs) {
-    // !schedule 5 30 minutes true !test testarg
-    if (sArgs.argsList.length < 5) {
+private boolean schedule(ScriptArgs sArgs) {
+    // !schedule set 5 30 minutes true !test testarg
+    if (sArgs.argsList.length < 6) {
         String commandStr = BuiltinCommands.GENERAL_INSUFFICIENT_ARGS + " " + sArgs.commandName;
         ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
         return false;
     }
     int offset;
     try {
-        offset = Integer.parseInt(sArgs.argsList[0]);
+        offset = Integer.parseInt(sArgs.argsList[1]);
     } catch (NumberFormatException e) {
         String commandStr = BuiltinCommands.GENERAL_INVALID_ARG + " " + sArgs.commandName + " " + sArgs.argsList[0];
         ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
@@ -32,21 +60,21 @@ public boolean execute(ScriptArgs sArgs) {
     }
     int period;
     try {
-        period = Integer.parseInt(sArgs.argsList[1]);
+        period = Integer.parseInt(sArgs.argsList[2]);
     } catch (NumberFormatException e) {
         String commandStr = BuiltinCommands.GENERAL_INVALID_ARG + " " + sArgs.commandName + " " + sArgs.argsList[1];
         ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
         return false;
     }
-    String timeUnit = sArgs.argsList[2].toUpperCase();
-    boolean reset = !sArgs.argsList[3].equals("false");
-    String command = String.join(" ", sArgs.argsList[4..-1]);
-    if (!Command.exists(sArgs.db, sArgs.argsList[4])) {
-        String commandStr = ResponseCmd.GENERAL_DOES_NOT_EXIST + " " + sArgs.argsList[4];
+    String timeUnit = sArgs.argsList[3].toUpperCase();
+    boolean reset = !sArgs.argsList[4].equals("false");
+    String command = String.join(" ", sArgs.argsList[5..-1]);
+    if (!Command.exists(sArgs.db, sArgs.argsList[6])) {
+        String commandStr = ResponseCmd.GENERAL_DOES_NOT_EXIST + " " + sArgs.argsList[5];
         ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
         return false;
     }
-    if (isScheduled(sArgs.channel, command)) {
+    if (APISchedule.isScheduled(sArgs.channel, command)) {
         String commandStr = ResponseCmd.COMMAND_ALREADY_SCHEDULED + " " + command;
         ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
         return false;
@@ -72,9 +100,26 @@ public boolean execute(ScriptArgs sArgs) {
             ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
             return false;
     }
-
 }
-private boolean isScheduled(String channel, String command) {
-    return APIChannel.get(channel).getScheduledCommands().containsKey(command)
 
+
+private boolean unSchedule(ScriptArgs sArgs) {
+    //!schedule remove !test
+    if (sArgs.argsList.length < 2) {
+        String commandStr = BuiltinCommands.GENERAL_INSUFFICIENT_ARGS + " " + sArgs.commandName;
+        ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
+        return false;
+    }
+    String command = String.join(" ", sArgs.argsList[1..-1]);
+    if (!APISchedule.isScheduled(sArgs.channel, command)) {
+        String commandStr = ResponseCmd.COMMAND_NOT_SCHEDULED + " " + command;
+        ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
+        return false;
+    }
+    APISchedule.unScheduleCommand(sArgs.channel, command);
+    APISchedule.removeFromDatabase(sArgs.channel, command);
+    String commandStr = ResponseCmd.SUCCESSFULL_UNSCHEDULE + " " + command;
+    ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
+    return true;
 }
+
