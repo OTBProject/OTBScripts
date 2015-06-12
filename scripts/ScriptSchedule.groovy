85f05d0916368ed645a671c85b69805d1709287e
NthPortal <mail.vanpelt@gmail.com>
2015-06-12 17:11:02 -0400

    Changed ScriptSchedule.groovy to use flags

--- scripts/ScriptSchedule.groovy
+++ scripts/ScriptSchedule.groovy
@@ -1,18 +1,20 @@
+import com.github.otbproject.otbproject.App
 import com.github.otbproject.otbproject.command.scheduler.Schedules
 import com.github.otbproject.otbproject.command.Commands
 import com.github.otbproject.otbproject.messages.send.MessagePriority
 import com.github.otbproject.otbproject.proc.ScriptArgs
 import com.github.otbproject.otbproject.util.BuiltinCommands
 import com.github.otbproject.otbproject.util.ScriptHelper
+import org.apache.logging.log4j.Level
 
 import java.util.concurrent.TimeUnit
 import java.util.stream.Collectors
 
 public class ResponseCmd {
-    public static final String SUCCESSFULL_SCHEDULE = "~%schedule.success";
+    public static final String SUCCESSFUL_SCHEDULE = "~%schedule.success";
     public static final String GENERAL_DOES_NOT_EXIST = "~%command.general:does.not.exist";
     public static final String COMMAND_ALREADY_SCHEDULED = "~%schedule.already.scheduled";
-    public static final String SUCCESSFULL_UNSCHEDULE = "~%unschedule.success";
+    public static final String SUCCESSFUL_UNSCHEDULE = "~%unschedule.success";
     public static final String COMMAND_NOT_SCHEDULED = "~%unschedule.not.scheduled";
 }
 public boolean execute(ScriptArgs sArgs) {
@@ -21,7 +23,16 @@ public boolean execute(ScriptArgs sArgs) {
         ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
         return false;
     }
-    switch (sArgs.argsList[0].toLowerCase()) {
+
+    String action = sArgs.argsList[0];
+
+    if (sArgs.argsList.length == 1) {
+        sArgs.argsList = new String[0];
+    } else {
+        sArgs.argsList = sArgs.argsList[1..-1];
+    }
+
+    switch (action.toLowerCase()) {
         case "add":
         case "new":
             return schedule(sArgs);
@@ -44,12 +55,32 @@ public boolean execute(ScriptArgs sArgs) {
 }
 
 private boolean schedule(ScriptArgs sArgs) {
-    // !schedule set 5 30 minutes true !test testarg
-    if (sArgs.argsList.length < 6) {
+    // OLD: !schedule set 5 30 minutes true !test testarg
+    // !schedule/!repeat set <flags> 30 5 !test testArg
+    if (sArgs.argsList.length < 3) {
         String commandStr = BuiltinCommands.GENERAL_INSUFFICIENT_ARGS + " " + sArgs.commandName;
         ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
         return false;
     }
+
+    TimeUnit timeUnit;
+    boolean reset;
+    try {
+        (timeUnit, reset) = getFlags(sArgs);
+    } catch (Exception e) {
+        App.logger.debug(e.getMessage(), Level.DEBUG);
+        return false;
+    }
+
+    int period;
+    try {
+        period = Integer.parseInt(sArgs.argsList[0]);
+    } catch (NumberFormatException e) {
+        String commandStr = BuiltinCommands.GENERAL_INVALID_ARG + " " + sArgs.commandName + " " + sArgs.argsList[1];
+        ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
+        return false;
+    }
+
     int offset;
     try {
         offset = Integer.parseInt(sArgs.argsList[1]);
@@ -58,18 +89,9 @@ private boolean schedule(ScriptArgs sArgs) {
         ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
         return false;
     }
-    int period;
-    try {
-        period = Integer.parseInt(sArgs.argsList[2]);
-    } catch (NumberFormatException e) {
-        String commandStr = BuiltinCommands.GENERAL_INVALID_ARG + " " + sArgs.commandName + " " + sArgs.argsList[1];
-        ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
-        return false;
-    }
-    String timeUnit = sArgs.argsList[3].toUpperCase();
-    boolean reset = !sArgs.argsList[4].equals("false");
-    String command = String.join(" ", sArgs.argsList[5..-1]);
-    if (!Commands.exists(sArgs.db, sArgs.argsList[6])) {
+
+    String command = String.join(" ", sArgs.argsList[2..-1]);
+    if (!Commands.exists(sArgs.db, sArgs.argsList[2])) {
         String commandStr = ResponseCmd.GENERAL_DOES_NOT_EXIST + " " + sArgs.argsList[5];
         ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
         return false;
@@ -79,20 +101,20 @@ private boolean schedule(ScriptArgs sArgs) {
         ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
         return false;
     }
-    switch (TimeUnit.valueOf(timeUnit)) {
+    switch (timeUnit) {
         case TimeUnit.MINUTES:
             Schedules.scheduleCommandInMinutes(sArgs.channel, command, offset, period, reset)
-            String commandStr = ResponseCmd.SUCCESSFULL_SCHEDULE + " " + command;
+            String commandStr = ResponseCmd.SUCCESSFUL_SCHEDULE + " " + command;
             ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
             return true;
         case TimeUnit.HOURS:
             Schedules.scheduleCommandInHours(sArgs.channel, command, offset, period)
-            String commandStr = ResponseCmd.SUCCESSFULL_SCHEDULE + " " + command;
+            String commandStr = ResponseCmd.SUCCESSFUL_SCHEDULE + " " + command;
             ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
             return true;
         case TimeUnit.SECONDS:
             Schedules.scheduleCommandInSeconds(sArgs.channel, command, offset, period, reset)
-            String commandStr = ResponseCmd.SUCCESSFULL_SCHEDULE + " " + command;
+            String commandStr = ResponseCmd.SUCCESSFUL_SCHEDULE + " " + command;
             ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
             return true;
         default:
@@ -118,8 +140,80 @@ private boolean unSchedule(ScriptArgs sArgs) {
     }
     Schedules.unScheduleCommand(sArgs.channel, command);
     Schedules.removeFromDatabase(sArgs.channel, command);
-    String commandStr = ResponseCmd.SUCCESSFULL_UNSCHEDULE + " " + command;
+    String commandStr = ResponseCmd.SUCCESSFUL_UNSCHEDULE + " " + command;
     ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
     return true;
+}
+
+private getFlags(ScriptArgs sArgs) throws Exception {
+    TimeUnit timeUnit = TimeUnit.MINUTES;
+    boolean reset = true;
+
+    if (sArgs.argsList.length == 0) {
+        return [timeUnit, reset];
+    }
+
+    boolean doneTimeUnit = false;
+    boolean doneReset = false;
+
+    String firstArg = sArgs.argsList[0];
+
+    while (firstArg.startsWith("--")) {
+        switch (firstArg.replaceFirst("--", "")) {
+            case "seconds":
+                if (doneTimeUnit) {
+                    invalidFlag(sArgs, firstArg)
+                }
+                timeUnit = TimeUnit.SECONDS;
+                doneTimeUnit = true;
+                break;
+            case "minutes":
+                if (doneTimeUnit) {
+                    invalidFlag(sArgs, firstArg)
+                }
+                timeUnit = TimeUnit.MINUTES;
+                doneTimeUnit = true;
+                break;
+            case "hours":
+                if (doneTimeUnit) {
+                    invalidFlag(sArgs, firstArg)
+                }
+                timeUnit = TimeUnit.HOURS;
+                doneTimeUnit = true;
+                break;
+            case "reset":
+                if (doneReset) {
+                    invalidFlag(sArgs, firstArg)
+                }
+                reset = true;
+                doneReset = true;
+                break;
+            case "no-reset":
+                if (doneReset) {
+                    invalidFlag(sArgs, firstArg)
+                }
+                reset = false;
+                doneReset = true;
+                break;
+            default:
+                invalidFlag(sArgs, firstArg)
+        }
+
+        // remove first arg or return if sArgs.argsList will be empty
+        if (sArgs.argsList.length == 1) {
+            return [timeUnit, reset]
+        }
+        else {
+            sArgs.argsList = sArgs.argsList[1..-1]
+            firstArg = sArgs.argsList[0];
+        }
+    }
+    return [timeUnit, reset]
+}
+
+private void invalidFlag(ScriptArgs sArgs, String firstArg) throws Exception {
+    String commandStr = BuiltinCommands.GENERAL_INVALID_FLAG + " " + sArgs.commandName + " " + firstArg;
+    ScriptHelper.runCommand(commandStr, sArgs.user, sArgs.channel, sArgs.destinationChannel, MessagePriority.HIGH);
+    throw new Exception("Invalid flag: " + firstArg);
 }
 
