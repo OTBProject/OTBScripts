239f52207a7e65df2cd1d311840a63ac2b2a2cc2
NthPortal <mail.vanpelt@gmail.com>
2015-06-21 06:26:33 -0400

    Fixed bot uptime script

--- scripts/ScriptBotUptime.groovy
+++ scripts/ScriptBotUptime.groovy
@@ -7,15 +7,10 @@ import java.lang.management.RuntimeMXBean
 
 public boolean execute(ScriptArgs sArgs) {
     RuntimeMXBean runtimeBean = ManagementFactory.getRuntimeMXBean();
-    long startTime = runtimeBean.getStartTime();
-    Date startDate = new Date(startTime);
-    Date nowDate = new Date();
-    long difference = nowDate.getTime() - startDate.getTime();
-    Date date = new Date(difference);
-    Calendar calendar = Calendar.getInstance();
-    calendar.setTime(date);
+    Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+    calendar.setTimeInMillis(runtimeBean.getUptime());
     int days = calendar.get(Calendar.DAY_OF_YEAR) - 1;
-    int hours = calendar.get(Calendar.HOUR) - 1;
+    int hours = calendar.get(Calendar.HOUR_OF_DAY);
     int mins = calendar.get(Calendar.MINUTE);
     int secs = calendar.get(Calendar.SECOND);
     StringBuilder builder = new StringBuilder("The bot has been up for: ");
@@ -26,6 +21,3 @@ public boolean execute(ScriptArgs sArgs) {
     ScriptHelper.sendMessage(sArgs.destinationChannel, builder.toString(), MessagePriority.HIGH)
     return true;
 }
-
-
-
