4889b2e39431c37c7ae90df24072c173df8dab5c
NthPortal <mail.vanpelt@gmail.com>
2015-06-21 05:38:41 -0400

    Tweaked uptime script

--- scripts/ScriptUptime.groovy
+++ scripts/ScriptUptime.groovy
@@ -10,16 +10,21 @@ public boolean execute(ScriptArgs sArgs) {
     long startTime = runtimeBean.getStartTime();
     Date startDate = new Date(startTime);
     Date nowDate = new Date();
-    long differnce = nowDate.getTime() - startDate.getTime();
-    Date date = new Date(differnce);
+    long difference = nowDate.getTime() - startDate.getTime();
+    Date date = new Date(difference);
     Calendar calendar = Calendar.getInstance();
     calendar.setTime(date);
     int days = calendar.get(Calendar.DAY_OF_YEAR) - 1;
     int hours = calendar.get(Calendar.HOUR) - 1;
     int mins = calendar.get(Calendar.MINUTE);
     int secs = calendar.get(Calendar.SECOND);
-    String response = days + (days == 1 ? " day" : " days") + ", " + hours + (hours == 1 ? " hour" : " hours") + ", " + mins + (mins == 1 ? " minute" : " minutes") + ", " + secs + (secs == 1 ? " second" : " seconds");
-    ScriptHelper.sendMessage(sArgs.destinationChannel, response, MessagePriority.HIGH)
+    StringBuilder builder = new StringBuilder("The bot has been up for: ");
+    builder.append(days + (days == 1 ? " day" : " days") + ", ");
+    builder.append(hours + (hours == 1 ? " hour" : " hours") + ", ");
+    builder.append(mins + (mins == 1 ? " minute" : " minutes") + ", ");
+    builder.append(secs + (secs == 1 ? " second" : " seconds"));
+    ScriptHelper.sendMessage(sArgs.destinationChannel, builder.toString(), MessagePriority.HIGH)
+    return true;
 }
 
 
